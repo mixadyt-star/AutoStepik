c2242661ead8e7a0b0504321726b0f33370e91d3
mixadyt-star <riseofkingdoms10kkmoschi@gmail.com>
2025-12-08 20:20:23 +0300

    Make better logger system

--- autostepik/logger.py
+++ autostepik/logger.py
@@ -1,14 +1,41 @@
 import logging
+import colorlog
 
-RESET = "\033[0m"
-INVERSE = "\033[7m"
-GREEN = "\033[32m"
-RED_BG = "\033[41m"
+logger = colorlog.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.handlers.clear()
 
-logging.basicConfig(
-    level=logging.INFO,
-    format=f"{GREEN}[ %(asctime)s %(filename)s:%(lineno)d %(levelname)s] %(message)s{RESET}",
-    datefmt="%y-%m-%d %H:%M:%S"
-)
+console_handler = colorlog.StreamHandler()
+console_handler.setFormatter(colorlog.ColoredFormatter(
+    fmt="%(log_color)s[%(asctime)s %(filename)s:%(lineno)d %(levelname)s] %(message)s",
+    datefmt="%y-%m-%d %H:%M:%S",
+    style="%",
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "bold_light_yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    },
+    reset=True,
+))
+console_handler.setLevel(logging.INFO)
 
-logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('autostepik.log')
+file_handler.setFormatter(logging.Formatter(
+    fmt="[ %(asctime)s %(filename)s:%(lineno)d %(levelname)s] %(message)s",
+    datefmt="%y-%m-%d %H:%M:%S",
+))
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logger.propagate = False
+
+if __name__ == "__main__":
+    logger.debug("DEBUG")
+    logger.info("INFO")
+    logger.warning("WARNING")
+    logger.error("ERROR")
+    logger.critical("CRITICAL")
